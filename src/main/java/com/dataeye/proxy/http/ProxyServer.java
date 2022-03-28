f20665201555338771f9c28080c74bfac8c56acd
wangchaojia@dataeye.com <wangchaojia@dataeye.com>
2022-03-28 12:10:26 +0800

    修改部署环境参数和README文件

--- src/main/java/com/dataeye/proxy/http/ProxyServer.java
+++ src/main/java/com/dataeye/proxy/http/ProxyServer.java
@@ -14,6 +14,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
