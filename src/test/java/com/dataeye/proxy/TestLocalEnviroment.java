1aecbf59037bdf1be371420ebb5e51512196ff3a
wangchaojia@dataeye.com <wangchaojia@dataeye.com>
2022-04-01 20:34:33 +0800

    修改mysql为线上数据库

--- src/test/java/com/dataeye/proxy/TestLocalEnviroment.java
+++ src/test/java/com/dataeye/proxy/TestLocalEnviroment.java
@@ -34,13 +34,17 @@ public class TestLocalEnviroment {
 
 //    private static final String pageUrl = "https://www.zhihu.com";
     private static final String pageUrl = "http://www.zhihu.com";
-    private static final String proxyIp = "127.0.0.1";
-    private static final int proxyPort = 8125;
-//    private static final String username = "t14480740933876";
-//    private static final String password = "wnwx5oeo";
 
+//    private static final String proxyIp = "127.0.0.1";
+//    private static final int proxyPort = 8125;
+//    private static final String username = "dataeye";
+//    private static final String password = "dataeye++123";
+
+    private static final String proxyIp = "tunnel-proxy-1-internet.de123.net";
+    private static final int proxyPort = 21330;
     private static final String username = "dataeye";
     private static final String password = "dataeye++123";
+
     private static final String root = System.getProperty("user.dir") + File.separator + "proxy_result";
 
     public static void main(String[] args) throws IOException {
